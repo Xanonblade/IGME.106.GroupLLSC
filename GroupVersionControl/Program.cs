7b15440cc99ef515722bf3110d98408bf568f595
Xanonblade <116854916+Xanonblade@users.noreply.github.com>
2026-01-16 14:35:05 -0500

    Laurin.3

--- GroupVersionControl/Program.cs
+++ GroupVersionControl/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-//I am groot
+//I am groot - Laurin Zanon
 
 // Lucas Kishore
 // Lucas wrote this comment
@@ -10,7 +10,44 @@ namespace GroupVersionControl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Problem #1: Dice Sum
+            DiceSum();
+        }
+
+        /// <summary>
+        /// Rolls 2d6 until user given sum is reached
+        /// </summary>
+        public static void DiceSum()
+        {
+            Random rng = new Random();
+
+            //Loop for valid sum
+            int sum = 0;
+            while (sum < 2 || sum > 12)
+            {
+                Console.Write($"Desired dice sum: ");
+                sum = int.Parse(Console.ReadLine()!);
+                if (sum >2 && sum < 12)
+                {
+                    //Valid
+                    break;
+                }
+                else
+                {
+                    sum = 0;
+                    Console.WriteLine("Invalid sum.\n");
+                }
+            }
+
+            //Rolling for desired sum
+            int rand1 = 0;
+            int rand2 = 0;
+            while ((rand1 + rand2) != sum)
+            {
+                rand1 = rng.Next(1, 7);
+                rand2 = rng.Next(1, 7);
+                Console.WriteLine($"{rand1} + {rand2} = {rand1 + rand2}");
+            }
         }
     }
 }
