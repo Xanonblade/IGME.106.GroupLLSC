bfae0e7f0e3592ad6929c07f4e08b5f9472dc419
Corey Stinson <corey@coreystinson.com>
2026-01-16 14:31:28 -0500

    Added IsPalindrome method and testing to main

--- GroupVersionControl/Program.cs
+++ GroupVersionControl/Program.cs
@@ -10,7 +10,35 @@ namespace GroupVersionControl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] words = { "racecar", "happy birthday", "tacocat", "programming" };
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (IsPalindrome(words[i]))
+                {
+                    Console.WriteLine($"{words[i]} is a palindrome!");
+                }
+                else
+                {
+                    Console.WriteLine($"{words[i]} is not a palindrome!");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks if a string is a palindrome
+        /// </summary>
+        /// <param name="sequence"></param>
+        /// <returns> Returns true if a string is a palindrome and false otherwise</returns>
+        public static bool IsPalindrome(string sequence)
+        {
+            string reversed = "";
+            sequence = sequence.ToLower();
+            for (int i = sequence.Length - 1; i >= 0; i--)
+            {
+                reversed += sequence[i];
+            }
+
+            return reversed == sequence;
         }
     }
 }
